96c6e65bd5f55de17bb129b6101906286f859564
Marco Täschner <marco.taeschner@me.com>
2020-11-24 20:55:34 +0100

    Update documentation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # smart-meter-sml
 Smart Meter - SML Parser
+
+## Requirements
+
+### Hardware
+- Smart meter with SML Output e. g.
+- Weidmann Elektronik [Optokopf](https://shop.weidmann-elektronik.de/index.php?page=product&info=24)
+- ELV [Smart Meter ES-IEC](README.mdhttps://de.elv.com/elv-homematic-energiesensor-fuer-smart-meter-es-iec-komplettbausatz-142148)
+- Raspberry Pi
+
+### Software (on Raspberry)
+- Bash
+- PHP
+
--- read-serial.sh
+++ read-serial.sh
@@ -7,10 +7,13 @@ fileext=_$(date +%Y%m%d%H%M%S).txt
 ### Binaer auf lesbar umstellen
 stty -F /dev/ttyUSB0 1:0:8bd:0:3:1c:7f:15:4:5:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0 &
  
-### Daten von serieller Schhnittstelle einlesen
+### Daten von serieller Schhnittstelle einlesen und in Datei schreiben (10 Sekunden lang)
 cat /dev/ttyUSB0 | od -tx1 > ${datapath}serialin${fileext}  &
 sleep 10
 kill -TERM $!
 
 ### Script zum Auswerten des SML-Datenstroms aufrufen
 php /home/pi/smart-home/sml.php type=DWS7420 smartMeterId=id-smart.meter url=https://localhost/api/v1/smartmeter/data token=1234567890 path=/smart-home/data/
+
+### Jede Minute per crontab starten
+# */1 * * * *  pi /home/pi/smart-home/read-serial.sh > /home/pi/smart-home/output.txt
